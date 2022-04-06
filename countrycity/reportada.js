2eebc941939860be31783c0132d071bcd35043e6
Tony <antonyitki@protonmail.com>
2022-04-06 13:15:12 +0100

    sprint4

--- countrycity/reportada.js
+++ countrycity/reportada.js
@@ -111,3 +111,53 @@ app.get('/person/:id', function(req, res) {
 app.listen(3000, function () {
     console.log('listening on port', 3000);
 });
+
+
+
+
+var express = require("express");
+var mysql = require("mysql");
+
+var app = express();
+
+// Create connections
+function DBConnection(){
+    return mysql.createConnection({
+        host    : 'localhost',
+        user    : "root",
+        password: "foobar",
+        database: 'world'
+    });
+}
+
+// Use pug as templating engine
+app.set('view engine', 'pug');
+
+app.get('/index', function(req,res){
+    var languagesList = [];
+
+    // Connect to database
+    var connection = DBConnection();
+    connection.connect();
+
+    connection.query('SELECT Language, Percentage FROM most_popular_languages',function(err, rows, fields){
+        if (err){
+            res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+        } else{
+            for (var i = 0; i < rows.length; i++){
+                var languages = {
+                    'language':rows[i].language,
+                    'percentage':rows[i].percentage
+                }
+                languagesList.push(languages)
+            }
+            res.render('index', {"languagesList": languagesList});
+        }
+    });
+    connection.end();
+});
+
+// Listen on port 8080
+app.listen(8080, function () {
+    console.log('listening on port', 8080);
+});
