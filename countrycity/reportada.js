0323dbeda8a03aba8f297f1e0ad59580af45984f
Your Name <you@example.com>
2022-04-07 16:22:51 +0000

    done

--- countrycity/reportada.js
+++ countrycity/reportada.js
@@ -1,3 +1,52 @@
-function myFunc(){
-    alert("You Clicked me");
- }
+//creating conecion to db
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'dbuser',
+  password: 's3kreee7',
+  database: 'my_db'
+})
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
+
+
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host: 'localhost', // Replace with your host name
+  user: 'root',      // Replace with your database username
+  password: '',      // Replace with your database password
+  database: 'my-node' // // Replace with your database Name
+}); 
+ 
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log('Database is connected successfully !');
+});
+module.exports = conn;
+
+
+
+
+
